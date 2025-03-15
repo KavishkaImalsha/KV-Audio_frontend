45dc946cfe8c6725e4e6711b9267fb19d1b8f137
Kavishka Imalsha <kavishkaimalsha@gmail.com>
2025-03-15 23:49:01 +0530

    Implement update product feature

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import AdminSideBar from '../../components/AdminSideBar.jsx'
 import AdminHome from './side bar actions/AdminHome.jsx'
 import Items from './side bar actions/Items.jsx'
+import UpdateItemModel from '../../components/admin/UpdateItemModel.jsx'
 const AdminDashboard = () => {
     return(
         <>
@@ -11,6 +12,7 @@ const AdminDashboard = () => {
               <Routes>
                   <Route path='/dashboard' element={<AdminHome/>}/>
                   <Route path='/items' element={<Items/>}/>
+                  <Route path='/edit-item' element={<UpdateItemModel/>}/>
                   <Route path='/bookings' element={<h1>Bookings</h1>}/>
                   <Route path='/users' element={<h1>Users</h1>}/>
               </Routes>
--- src/pages/admin/side bar actions/Items.jsx
+++ src/pages/admin/side bar actions/Items.jsx
@@ -58,6 +58,10 @@ const Items = () => {
             }
           })
     }
+
+    const updateItem = (product) => {
+        navigate('/admin/edit-item', {state: product})
+    }
     
 
     const handelSubmit = async(event) => {
@@ -84,35 +88,35 @@ const Items = () => {
             </button>
             {isModalVisible && <AddItemModel showModel={setIsModalVisible} setItem={setItemDetails} submitForm={handelSubmit}/>}
             {itemsLoading ? (
-                <div class="shadow-md sm:rounded-lg w-full">
-                <table class="w-full text-sm text-left rtl:text-right text-gray-500 ">
-                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 ">
+                <div className="shadow-md sm:rounded-lg w-full">
+                <table className="w-full text-sm text-left rtl:text-right text-gray-500 ">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 ">
                         <tr>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-6 py-3">
                                 Product ID
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-6 py-3">
                                 Product Name
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-6 py-3">
                                 Product Category
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-6 py-3">
                                 Unit Price
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-6 py-3">
                                 dimension
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-6 py-3">
                                 Description
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-6 py-3">
                                 Availability
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-6 py-3">
                                 Quantity
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-6 py-3">
                                 Actions
                             </th>
                         </tr>
@@ -120,34 +124,34 @@ const Items = () => {
                     <tbody>
                         {products.map((product, index) => {
                             return(
-                                <tr key={index} class="bg-white border-b border-gray-200 hover:bg-gray-50 ">
-                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                <tr key={index} className="bg-white border-b border-gray-200 hover:bg-gray-50 ">
+                                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                         {product.productId}
                                     </th>
-                                    <td class="px-6 py-4">
+                                    <td className="px-6 py-4">
                                         {product.name}
                                     </td>
-                                    <td class="px-6 py-4">
+                                    <td className="px-6 py-4">
                                         {product.category}
                                     </td>
-                                    <td class="px-6 py-4">
+                                    <td className="px-6 py-4">
                                         {product.price}
                                     </td>
-                                    <td class="px-6 py-4">
+                                    <td className="px-6 py-4">
                                         {product.dimension}
                                     </td>
-                                    <td class="px-6 py-4">
+                                    <td className="px-6 py-4">
                                         {product.discription}
                                     </td>
-                                    <td class={`px-6 py-4 ${product.availability ? "text-green-600" : "text-red-600"}`}>
+                                    <td className={`px-6 py-4 ${product.availability ? "text-green-600" : "text-red-600"}`}>
                                         {product.availability ? "Yes" : "No"}
                                     </td>
-                                    <td class="px-6 py-4">
+                                    <td className="px-6 py-4">
                                         {product.quantity}
                                     </td>
-                                    <td class="px-6 py-4 text-right">
-                                        <button class="hover:cursor-pointer font-medium text-blue-600 hover:underline">Edit</button>|
-                                        <button onClick={() => {deleteItem(product._id)}} class="hover:cursor-pointer font-medium text-red-600 hover:underline">Delete</button>
+                                    <td className="px-6 py-4 text-right">
+                                        <button onClick={() => {updateItem(product)}} className="hover:cursor-pointer font-medium text-blue-600 hover:underline">Edit</button>|
+                                        <button onClick={() => {deleteItem(product._id)}} className="hover:cursor-pointer font-medium text-red-600 hover:underline">Delete</button>
                                     </td>
                                 </tr>
                             )
