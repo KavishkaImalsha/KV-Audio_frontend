8a96c218c4596c1730a0c4b626d38feb6cbf4ed0
Kavishka Imalsha <kavishkaimalsha@gmail.com>
2025-07-16 12:58:37 +0530

    Send formData to backend for store

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,35 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import Footer from "../components/Footer"
 import MapLocation from "../components/map/MapLocation"
+import { useState } from "react"
+import handleInputData from "../actions/handleInputData"
+import BackendApi from "../api/BackendApi"
+import toast from "react-hot-toast"
 
 const Contact = () => {
+    const [formData, setFormData] = useState({name: "", email: "", phone: "", subject: "", message: ""})
+    const navigate = useNavigate()
+    
+    const sendMessage = async(event) => {
+        event.preventDefault()
+
+        try{
+            const token = localStorage.getItem("token")
+        
+            if(token){
+                const inquiryRes = await BackendApi.post("/inquiries", formData)
+                toast.success(inquiryRes.data.message)
+                navigate('/items')
+            }else{
+                navigate('/signup')
+            }
+        }catch(error){
+            toast.error(error?.response?.data?.message || "Error Occured")
+        }
+
+        
+        
+    }
     return(
         <>
             <div>
@@ -10,24 +37,24 @@ const Contact = () => {
                 <div className="grid grid-cols-2 my-3 mx-15">
                     <div className="mx-5">
                         <h1 className="text-3xl font-quicksand my-2">Contact Us</h1>
-                        <form>
+                        <form onSubmit={(event) => {sendMessage(event)}}>
                             <div className="grid grid-cols-2 gap-2 mb-5">
                                 <div>
                                     <label htmlForfor="name" className="block">Your Name</label>
-                                    <input type="text" name="name" className="w-full h-10 px-3 border border-gray-300 focus:outline-0" required/>
+                                    <input onChange={(event) => {handleInputData(event, setFormData)}} type="text" name="name" className="w-full h-10 px-3 border border-gray-300 focus:outline-0" required/>
                                 </div>
                                 <div>
                                     <label htmlForfor="email" className="block">Your Email</label>
-                                    <input type="text" name="email" className="w-full h-10 px-3 border border-gray-300 focus:outline-0" required/>
+                                    <input onChange={(event) => {handleInputData(event, setFormData)}} type="text" name="email" className="w-full h-10 px-3 border border-gray-300 focus:outline-0" required/>
                                 </div>
                             </div>
                             <div className="mb-5">
                                 <label htmlForfor="phone" className="block">Mobile number</label>
-                                <input type="number" name="phone" className="w-[50%] h-10 px-3 border border-gray-300 focus:outline-0" required/>
+                                <input onChange={(event) => {handleInputData(event, setFormData)}} type="number" name="phone" className="w-[50%] h-10 px-3 border border-gray-300 focus:outline-0" required/>
                             </div>
                             <div className="mb-5">
                                 <label htmlForfor="subject" className="block">Subject</label>
-                                <select className="w-full h-10 px-3 border border-gray-300 focus:outline-0">
+                                <select onChange={(event) => {handleInputData(event, setFormData)}} name="subject" className="w-full h-10 px-3 border border-gray-300 focus:outline-0">
                                     <option>Select Subject</option>
                                     <option>General Support</option>
                                     <option>Thecnical Support</option>
@@ -36,7 +63,7 @@ const Contact = () => {
                             </div>
                             <div>
                                 <label htmlForfor="message" className="block">Message</label>
-                                <textarea name="message" className="w-full h-56 py-3 px-3 border border-gray-300 focus:outline-0" required/>
+                                <textarea onChange={(event) => {handleInputData(event, setFormData)}} name="message" className="w-full h-56 py-3 px-3 border border-gray-300 focus:outline-0" required/>
                             </div>
                             <button type="submit" className="bg-black text-white p-3 my-2 hover:bg-gray-900 hover:cursor-pointer">Send Message</button>
                         </form>
